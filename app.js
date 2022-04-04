9a664012d98aa6b301582d3511527eaa2806dc11
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-04-04 22:33:11 +0530

    ejs begins

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express")
+const bodyParser= require("body-parser")
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
 
 let today= new Date()
 app.get('/',(req,res)=>{
     //res.sendFile(path.join(__dirname, '/index.html')); to send a html file
     res.send(today)
 })
-
 
 app.listen(port, ()=>{
     console.log("Listening")
