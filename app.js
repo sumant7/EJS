9beceabe9bbc544ecec382dd9eb9431061dd534c
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-04-04 22:45:03 +0530

    ejs basic

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 let today= new Date()
+let current = today.getDate()
 app.get('/',(req,res)=>{
-    //res.sendFile(path.join(__dirname, '/index.html')); to send a html file
-    res.send(today)
+    //res.sendFile(__dirname + '/index.html'); //to send a html file
+    //res.send(today)
+    res.render("list",{day: current})  //to render list.ejs
+    //we sent the current variable with the name day
 })
-
 app.listen(port, ()=>{
     console.log("Listening")
 })
